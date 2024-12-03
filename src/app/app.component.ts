791f52307d818af5153bff03800236e94c6fdbb3
Kuntal Banik <deisiduke1986@gmail.com>
2024-12-03 08:14:06 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,3 +11,31 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'FrontDev';
 }
+
+let items = [250, 645, 300, 900, 50];
+let anotherNum = [350, 450, 850, 60]
+let names = ["John", "Hulu", "Jane"]
+
+for(let i=0; i < items.length; i++){
+  let offer = items[i] / 10;
+  items[i] -= offer;
+  // console.log(items[i]);
+}
+
+console.log(items);
+items.push(90);
+console.log(items);
+let poped = items.pop();
+console.log("Items poped : ", poped, items);
+
+// Add items before array
+items.unshift(2000);
+console.log(items);
+
+// Delete items from start of array
+items.shift();
+console.log(items);
+
+// Concat
+console.log(items.concat(anotherNum));
+
